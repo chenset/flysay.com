92c1ff8368cdf53180a93a262ca640f7b245ff99
chenset <4199191@qq.com>
2019-05-17 16:09:16 +0800

    update

--- random.php
+++ random.php
@@ -1,3 +1,4 @@
 <?php
-$files = glob('images/*.*');
-header('Location: ' . $files[array_rand($files)], true, 302);
+if ($files = glob('images/*.*')) {
+    header('Location: ' . (stristr($_SERVER['HTTP_REFERER'], 'flysay.com') ? '//img.flysay.com/' : '') . $files[array_rand($files)], true, 302);
+}
