dbb216a9dc877eb1863f8e87c849e133977d19b2
chenset <4199191@qq.com>
2019-05-21 08:49:30 +0800

    update

--- base.js
+++ base.js
@@ -40,7 +40,7 @@ function ajax(option) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
-            if (this.status === 200) {
+            if (this.status === 200 || this.status === 304) {
                 success && success(this);
             } else {
                 error && error(this);
